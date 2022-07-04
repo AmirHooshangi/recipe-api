2f0a7d9551b968bf104ef9ef470874f7e4e888e2
amirhoshangi <amirhoshangi@gmail.com>
2022-07-05 01:43:43 +0430

    Spring boot based structure for the project

---
+++ src/main/java/com/abn/recipe/RecipeApiApplication.java
@@ -0,0 +1,32 @@
+package com.abn.recipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@EnableSwagger2
+@SpringBootApplication
+@ComponentScan("com.abn.*")
+@EntityScan("com.abn.*")
+@EnableJpaRepositories("com.abn.*")
+public class RecipeApiApplication implements WebMvcConfigurer {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html")
+                .addResourceLocations("classpath:/META-INF/resources/");
+
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipeApiApplication.class, args);
+	}
+
+}
